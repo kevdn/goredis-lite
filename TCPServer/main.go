f59824cc5d4a655a64881f3f02c31a1adc97c52d
kevdn <lehuukhoa12@gmail.com>
2025-09-22 23:35:25 +0700

    feat(TCPServer): refactor TCP server to support persistent client sessions

--- TCPServer/main.go
+++ TCPServer/main.go
@@ -3,20 +3,43 @@ package main
 import (
 	"log"
 	"net"
-	"time"
+	"io"
+	"fmt"
 )
 
 func handleConnection(conn net.Conn) {
-	defer conn.Close()
-	log.Println(conn.RemoteAddr())
-	var buf []byte = make([]byte, 1000)
-	_, err := conn.Read(buf)
-	if err != nil {
-		log.Fatal(err)
+	log.Println("Handling connection from", conn.RemoteAddr())
+	for {
+		cmd, err := readCommand(conn)
+		if err != nil {
+			conn.Close()
+			log.Println("Client disconnected", conn.RemoteAddr())
+			if err == io.EOF {
+				break
+			}
+		}
+		if err = respond(cmd, conn); err != nil {
+			log.Println("Error responding to client", conn.RemoteAddr())
+		}
 	}
-	time.Sleep(time.Second * 2)
-	log.Println("write response")
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\nHello!\r\n"))
+	
+}
+
+func readCommand(conn net.Conn) (string, error) {
+	var buf []byte = make([]byte, 512)
+	n, err := conn.Read(buf)
+	if err != nil {
+		return "", err
+	}
+	return string(buf[:n]), nil
+}
+
+func respond(cmd string, conn net.Conn) error {
+	cmd = fmt.Sprintf("+%s\r\n", cmd)
+	if _, err := conn.Write([]byte(cmd)); err != nil {
+		return err
+	}
+	return nil
 }
 
 func main() {
